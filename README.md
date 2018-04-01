35fffb4b7fd91af0762b8b2b96b18130b7acbd93
Khyati Kulshreshtha <khyati.kul@gmail.com>
2018-04-01 19:51:07 +0400

    deployed on glitch

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # URL Shortener
 FreeCodeCamp backend project
+
+Project is hosted on [https://cheerful-asterisk.glitch.me]
+Enter the url after /new/ 
+a JSON will be returned in the format`{ original url:"https://google.com", short url:"https://cheerful-asterisk.glitch.me/short/0" }`.
+
+The short URL will redirect to the original URL.
--- server.js
+++ server.js
@@ -1,53 +1,43 @@
-"use strict"
 
-//dependencies
-const express = require("express"),
-      env = require('dotenv').config();
-const R=require("ramda");
-const Task = require("data.task");
-//Local dependencies
-const Urls = require('./models/Urls')
-//global configuration
+
+// dependencies
+const express = require('express'),
+  env = require('dotenv').config(),
+  randomize = require('randomatic');
+
+// Local dependencies
+const Urls = require('./models/Urls');
+// global configuration
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/",(req,res) => {
-  const result ="enter url after /new/";
-res.send(result);
+app.get('/', (req, res) => {
+  const result = 'enter url after /new/';
+  res.send(result);
 });
 
-//const url = new Urls({ url : 'https:reddit.com', short :'1'});
-    
-   app.get("/new/:name(*)",(req,res) => {
-    console.log(req.params.name);
-    //check if it's valid url
-    Urls.find({ url: req.params.name},(err, a)=> {
-      if (err) return console.error(err);
-      //console.log(a);
-      //console.log(typeof a);
-      let result = a[0];
-      console.log(req.originalUrl);
-      res.send({"original url":result.url , "short url": `https://localhost:3000/short/${result.short}`})
-    })
-    })
-   app.get("/short/:name(*)",(req,res) => {
-//         console.log(req.params.name);
-         console.log(req.originalUrl);
+app.get('/new/:name(*)', (req, res) => {
+  console.log(req.params.name);
+  // check if it's valid url
+  Urls.find({ url: req.params.name })
+    .then(a => {
+    const result = a[0];
+    res.send({ 'original url': result.url, 'short url': `${req.protocol}://${req.get('host')}/short/${result.short}` });
+  })
+  .catch(err =>{
+    let u = req.params.name
+      const s = new Urls({ url: u, short:randomize('0',4)});
+      s.save((e,s) => console.log('added to db'));
+    });
+});
+app.get('/short/:name(*)', (req, res) => {
+  Urls.find({ short: req.params.name }, (err, a) => {
+    // TODO add site from db
+    const result = a[0];
+    res.redirect(result.url);
+  });
+});
 
-        Urls.find({short : req.params.name},(err,a) =>{
-          //TODO add site from db
-          let result =a[0];
-          res.redirect(result.url);
-        });
-  //       //check if in db
-  //       //redirect, else error
-  //       //res.redirect();
-      });
- 
-// const result = (url,short) => (url)
-// ?{"original_url":url,"short_url":`http:localhost:3000/short/${short}`}
-// :"invalid URL";
-
-app.listen(PORT,()=>
-console.log("server started at port %d.",PORT)
+app.listen(PORT, () =>
+  console.log('server started at port %d.', PORT),
 );
